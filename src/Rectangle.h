0411eaeedfc6ded5c828d61455b432eb619c4bfc
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-31 20:57:33 +0200

    Add name and setters

--- src/Rectangle.h
+++ src/Rectangle.h
@@ -13,16 +13,20 @@ class Rectangle: public PhysicsObject {
 private:
     double width, height;
 public:
-    Rectangle(const Vector& position, const Vector& velocity, const double& width,
+    Rectangle(const std::string& name, const Vector& position, const Vector& velocity, const double& width,
               const double& height)
-    : PhysicsObject(position, velocity), width(width), height(height) {}
+    : PhysicsObject(name, position, velocity), width(width), height(height) {}
 
-    Rectangle(const double& x, const double& y, const double& w, const double& h)
-    : PhysicsObject(Vector(x, y), Vector(0, 0)), width(w), height(h) {}
+    Rectangle(const std::string& name, const double& x, const double& y, const double& w, const double& h)
+    : PhysicsObject(name, Vector(x, y), Vector(0, 0)), width(w), height(h) {}
+
+    ~Rectangle(){ std::cout << "Destroying Rectangle" << std::endl; }
 
     [[nodiscard]] inline double getWidth() const { return width; }
-
     [[nodiscard]] inline double getHeight() const { return height; }
+
+    inline void setWidth(const double& w) { width = w; }
+    inline void setHeight(const double& h) { height = h; }
 
     void Render(SDL_Renderer* render) const override {
         (void)render;
