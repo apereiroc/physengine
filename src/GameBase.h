8ce2c65e497a6e56470c3f0ad09148ff68d50962
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-09-01 00:31:48 +0200

    Add GameBase class

---
+++ src/GameBase.h
@@ -0,0 +1,41 @@
+//
+// Created by Asier Pereiro Castro on 31/8/23.
+//
+
+#pragma once
+
+#include <memory>
+#include "PhysicsEngine.h"
+#include "RenderingEngine.h"
+#include "Rectangle.h"
+
+enum Shape {
+    Rectangle,
+
+    NUMSHAPES
+};
+
+class GameBase {
+protected:
+    std::unique_ptr<PhysicsEngine> phys;
+    std::unique_ptr<RenderingEngine> rend;
+public:
+    GameBase()
+    : phys(new PhysicsEngine()),
+      rend(new RenderingEngine()) {}
+
+    template<typename ...Args>
+    inline void addShape(const Shape& s, const std::string& name, const Args&... args) {
+        switch (s) {
+            case Rectangle:
+                phys->addObject(new class Rectangle(name, args...));
+                break;
+            default:
+                exit(1);
+        }
+    }
+
+    // Make class abstract. Each derivation should implement the game and end conditions
+    virtual void Run() const = 0;
+    [[nodiscard]] virtual bool endCondition() const = 0;
+};
