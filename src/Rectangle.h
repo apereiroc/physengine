7c221e1074b2490083a847edcb44da840eae3a42
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-09-01 00:32:46 +0200

    Improve constructor (fixing bug)

--- src/Rectangle.h
+++ src/Rectangle.h
@@ -8,19 +8,24 @@
 #include "Types.h"
 #include <SDL2/SDL.h>
 
+#include <utility>
+
 
 class Rectangle: public PhysicsObject {
 private:
     double width, height;
+
 public:
-    Rectangle(const std::string& name, const Vector& position, const Vector& velocity, const double& width,
+    Rectangle(std::string name, const Vector& position, const Vector& velocity, const double& width,
               const double& height)
-    : PhysicsObject(name, position, velocity), width(width), height(height) {}
+    : PhysicsObject(std::move(name), position, velocity), width(width), height(height) {}
 
-    Rectangle(const std::string& name, const double& x, const double& y, const double& w, const double& h)
-    : PhysicsObject(name, Vector(x, y), Vector(0, 0)), width(w), height(h) {}
+    Rectangle(std::string name, const double& x, const double& y, const double& w, const double& h)
+    : PhysicsObject(std::move(name), Vector(x, y), Vector(0, 0)), width(w), height(h) {}
 
-    ~Rectangle(){ std::cout << "Destroying Rectangle" << std::endl; }
+    Rectangle(std::string name, const double& x, const double& y, const double& w, const double& h,
+              const Colors& c)
+            : PhysicsObject(std::move(name), Vector(x, y), Vector(0, 0), c), width(w), height(h) {}
 
     [[nodiscard]] inline double getWidth() const { return width; }
     [[nodiscard]] inline double getHeight() const { return height; }
@@ -28,7 +33,12 @@ public:
     inline void setWidth(const double& w) { width = w; }
     inline void setHeight(const double& h) { height = h; }
 
-    void Render(SDL_Renderer* render) const override {
-        (void)render;
+    void Render(SDL_Renderer* renderer) const override {
+        const Vector &pos = getPosition();
+        SDL_Rect rect = {(int)pos.x(), (int)pos.y() - (int)height, (int)width, (int)height};
+        const Color &c = getColor();
+
+        SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+        SDL_RenderFillRect(renderer, &rect);
     }
 };
