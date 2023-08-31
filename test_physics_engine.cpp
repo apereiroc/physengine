a18b9ac3ea442de68a6185a25c07abb0855adbd9
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-31 20:57:49 +0200

    Improve test for physics engine

--- test_physics_engine.cpp
+++ test_physics_engine.cpp
@@ -12,22 +12,22 @@ int main(){
     auto p {std::make_unique<PhysicsEngine>()};
 
     // Create and add rectangle
-    auto r {std::make_unique<Rectangle>(0, 0, 50, 100)};
-    p->addObject(r.get());
+    const std::string name = "Rectangle 1";
+    p->addObject(new Rectangle(name, 0, 0, 50, 100));
 
     // Define a time step
     const double dt = 0.3;
 
     // Position should be (0,0), since the velocity is 0
     p->Simulate(dt);
-    const Vector& p0 = p->getObject(0)->getPosition();
+    const Vector& p0 = p->getPosition(name);
     std::cout << "Position: " << p0.x() << ", " << p0.y() << std::endl;
 
     // Add movement. Position should change
-    p->getObject(0)->setVelocity(3,-1);
+    p->setVelocity(name, 3,-1);
     p->Simulate(dt);
 
-    const Vector& p1 = p->getObject(0)->getPosition();
+    const Vector& p1 = p->getPosition(name);
     std::cout << "Position: " << p1.x() << ", " << p1.y() << std::endl;
 
 
