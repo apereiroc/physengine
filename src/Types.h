df8387855dbb84ed26cb0a22847c60c2f56c6c5d
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-31 18:57:15 +0200

    Update Color structure

--- src/Types.h
+++ src/Types.h
@@ -11,7 +11,10 @@ typedef Eigen::Vector2d Vector;
 typedef struct Color
 {
     unsigned char r, g, b, a;
-    Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) : r(r), g(g), b(b), a(a) {}
+    Color(): r(0), g(0), b(0), a(255) {};
+    Color(unsigned char r, unsigned char g,
+          unsigned char b, unsigned char a) :
+          r(r), g(g), b(b), a(a) {}
 } Color;
 
 enum Colors
